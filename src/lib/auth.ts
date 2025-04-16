1990ae62399333eb598d9967aff141394ec9640a
WAHoltz <wah.holtz@gmail.com>
2025-04-16 17:07:09 -0400

    Created initial Next project with basic Firebase config

---
+++ src/lib/auth.ts
@@ -0,0 +1,7 @@
+import { NextAuthOptions } from 'next-auth';
+import { CredentialsProvider } from 'next-auth/providers/credentials';
+
+export const authConfig: NextAuthOptions = {
+  providers: [
+  ],
+};
