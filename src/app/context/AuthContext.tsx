81c27eb1d88e11d18f60413b752e5a2958adb664
William Holtz <william.holtz@phoenix.edu>
2025-04-21 16:45:29 -0400

    changed usage of auth handlers

--- src/app/context/AuthContext.tsx
+++ src/app/context/AuthContext.tsx
@@ -1,12 +1,10 @@
 'use client'
-import { createContext, useContext, useState, useEffect } from "react";
-import { signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth";
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider, User } from "firebase/auth";
 import { auth } from "../firebase";
 
-const AuthContext = createContext()
-
-export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
+export const AuthContext = () => {
+  const [user, setUser] = useState<User | null>(null)
 
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider()
@@ -18,19 +16,18 @@ export const AuthContextProvider = ({ children }) => {
   }
 
 
-  const handleSignIn = () => {
+  const handleSignIn = async () => {
     try {
-      googleSignIn()
+      await googleSignIn()
 
     } catch (error) {
       console.log(error)
     }
   }
 
-  const handleSignOut = () => {
-    const { logOut } = UserAuth()
+  const handleSignOut = async () => {
     try {
-      logOut()
+      await logOut()
     } catch (error) {
       console.log(error)
     }
@@ -42,6 +39,11 @@ export const AuthContextProvider = ({ children }) => {
     })
     return () => unsubscribe()
   }, [user])
+
+  return createContext({ user, handleSignIn, handleSignOut })
+}
+
+export const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <AuthContext.Provider value={{ user, handleSignIn, handleSignOut }}>{children}</AuthContext.Provider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
 
   return (
     <div className='tw:flex tw:justify-center tw:align-middle tw:mt-48'>
-      {user ? (
+      {!user ? (
         <div className='tw:flex tw:justify-center tw:align-middle tw:items-center tw:flex-col tw:border-2 tw:rounded-md tw:w-2xl'>
           <h4 className="tw:m-4 tw:p-4 tw:text-3xl tw:font-bold">Log in with Google</h4>
           <Link className='tw:m-4 tw:bg-gray-500 tw:rounded-md tw:px-4 tw:py-2 tw:text-white' href='/home' onClick={handleSignIn}>
