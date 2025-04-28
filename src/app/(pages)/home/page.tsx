a86ced7095d119d01ecc62947f9dcdeabfcd6fc8
William Holtz <william.holtz@phoenix.edu>
2025-04-27 21:02:54 -0400

    implemented AddExpense function

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,14 +1,14 @@
-'use client';
-import { ProviderValue, UserAuth } from '@/app/context/AuthContext';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+"use client";
+import { ProviderValue, UserAuth } from "@/app/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const { user } = UserAuth() as ProviderValue;
   const router = useRouter();
 
   useEffect(() => {
-    if (!user) return router.push('/');
+    if (!user) return router.push("/");
     return;
   }, [user, router]);
 
--- src/app/db-utils/index.ts
+++ src/app/db-utils/index.ts
@@ -1,17 +1,27 @@
-'use server';
+"use server";
+
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase";
+
+export type Expense = {
+  name: string;
+  amount: number;
+  required: boolean;
+  userId: string;
+};
 
 export const AddUser = async () => {};
 
-//export const AddExpense = async (formData: object) => {
-//  const collectionRef = collection(db, 'expenses')
-//
-//  addDoc(collectionRef, {
-//    user:
-//})
-//}
-//
+export const AddExpense = async (formData: Expense) => {
+  const expenseRef = collection(db, "expenses");
+
+  await addDoc(expenseRef, {
+    ...formData,
+  });
+};
+
 //export const GetExpense = async (userId?: string, expenseId?: string) => {
-//  const collectionRef = collection(db, 'expenses');
+//  const collectionRef = collection(db, "expenses");
 //
 //  const collectionSnapshot = await getDocs(collectionRef);
 //};
