f96ee8e0ce7acb5b363150d9e538ad138f05c065
William Holtz <william.holtz@phoenix.edu>
2025-05-15 00:11:52 -0400

    added twMerge, expense and listexpense components, moved addform and listexpense to expenses page

--- src/app/(pages)/expenses/page.tsx
+++ src/app/(pages)/expenses/page.tsx
@@ -1,3 +1,25 @@
+'use client';
+import AddForm from '@/app/components/AddForm';
+import ListExpenses from '@/app/components/ListExpenses';
+import { ProviderValue, UserAuth } from '@/app/context/AuthContext';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 export default function Expenses() {
-  return <h1>Expenses</h1>;
+  const { user } = UserAuth() as ProviderValue;
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) return router.push('/');
+  }, [user, router]);
+
+  if (!user) {
+    return;
+  }
+  return (
+    <div className="tw:flex tw:items-center tw:justify-center tw:mt-2 tw:flex-col tw:space-y-3">
+      <AddForm userId={user.uid} />
+      <ListExpenses />
+    </div>
+  );
 }
